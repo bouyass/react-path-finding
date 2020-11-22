4032000b7e327389015d516e6059ab7c9d9a4f57
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-11-22 17:50:38 +0100

    commit event listeners

--- src/Board.js
+++ src/Board.js
@@ -1,17 +1,59 @@
-import React, {useEffect, useState} from 'react'
+import React, {useCallback, useEffect, useState, useRef} from 'react'
 import './Board.css'
 
 function Board() {
 
-    const [board, setBoard] = useState(new Array(26).fill(new Array(64).fill(0)))
+    const [board, setBoard] = useState(Array.from(Array(26), ()=> {return Array(64).fill(0)}))
+    const startIndex = useRef([])
+    const destinationIndex = useRef([])
 
+    const eventListener = (e) => {
+        let newBoard = board
+        let ids = e.target.id.split("-")
+        if(e.target.style.backgroundColor === ''){
+            newBoard[Number.parseInt(ids[0])][Number.parseInt(ids[1])] = 1
+            setBoard(newBoard)
+            e.target.style.backgroundColor = "#3f454e";
+            // if the start cell is not defined yet, we define it firstly
+            if(startIndex.current.length === 0){
+                let tempArray = [Number.parseInt(ids[0]), Number.parseInt(ids[1])]
+                startIndex.current = tempArray
+                e.target.style.backgroundColor = "green";
+            }else if(destinationIndex.current.length === 0){
+                // if the destination is not defined yet, and the start cell is already defined
+                destinationIndex.current = [Number.parseInt(ids[0]), Number.parseInt(ids[1])]
+                e.target.style.backgroundColor = "red";
+            }
+        }else{
+
+            if(JSON.stringify(startIndex) === JSON.stringify([Number.parseInt(ids[0]), Number.parseInt(ids[1])])){
+                // start index click
+                startIndex.current = []
+            }else if(JSON.stringify(startIndex) === JSON.stringify([Number.parseInt(ids[0]), Number.parseInt(ids[1])])){
+                // destination index click
+                destinationIndex.current = []
+            }
+            e.target.style.backgroundColor = "";
+            newBoard[Number.parseInt(ids[0])][Number.parseInt(ids[1])] = 0
+            setBoard(newBoard)
+        }
+
+    }
+
+    
     useEffect(() => {
-        console.log("lyes")
+        board.map((row, rowIndex) => row.map((column, columnIndex) => {
+            let id = `${rowIndex}-${columnIndex}`
+            document.getElementById(id).addEventListener('click', eventListener )
+        })) 
     }, [])
 
     return (
         <div className="boardContainer">
-            {board.map(row => row.map((cell,x) => <div className="cell">  </div>))}
+            {board.map((row, rowIndex )=> row.map((cell,columnIndex) => {
+                let id = `${rowIndex}-${columnIndex}`
+                return <div id={id} className="cell">   </div>
+        }))}
         </div>
     )
 }
